1307316d318ffb74ffd52d01d26a0eb990d242dc
Guillaume Wenzek <guw@fb.com>
2019-07-10 17:29:37 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="func_argparser",
+    version="1.0.0",
+    description="Generate CLI ArgumentParser from a function signature.",
+    author="gwenzek",
+    packages=["func_argparser"],
+    install_requires=[],
+)
