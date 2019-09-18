e88d679edf58de575abce40dbe106da98f4b1d6b
Guillaume Wenzek <guw@fb.com>
2019-09-18 17:32:44 +0200

    add README.md to "long_description"

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
+from pathlib import Path
 
+
+with open(Path(__file__).parent / "README.md") as f:
+    long_description = f.read()
 
 setup(
     name="func_argparse",
-    version="1.0.0",
     description="Generate CLI ArgumentParser from a function signature.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gwenzek/func_argparse",
+    version="1.0.1",
     author="gwenzek",
     packages=["func_argparse"],
+    license="BSD",
     install_requires=[],
 )
